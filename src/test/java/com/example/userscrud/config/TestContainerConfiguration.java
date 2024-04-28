8513f34eadfe87407313b703be9e2a11614310d7
Artem Kurkin <kurkin566@gmail.com>
2024-04-28 03:21:00 +0300

    Added testcontainers (#2)
    * fix application.yml
    
    * added testcontainers

---
+++ src/test/java/com/example/userscrud/config/TestContainerConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.userscrud.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration
+public class TestContainerConfiguration {
+
+    @Bean
+    @ServiceConnection
+    public PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>("postgres:16.0");
+    }
+
+}
